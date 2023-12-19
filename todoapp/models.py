ac88b390e8f571168e4ef2d452028087b61958b1
Deivison Washington <deivisonwashington@hotmail.com>
2023-12-19 02:42:41 -0300

    criado classe Task

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    name = models.CharField(max_length=80)
+    description = models.TextField(
+        max_length=150,
+        blank=True,
+        default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
