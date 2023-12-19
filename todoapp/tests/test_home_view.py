464cea0eb324fad936d30ef30ff87215549a70b0
Deivison Washington <deivisonwashington@hotmail.com>
2023-12-19 20:50:05 -0300

    test: testando view home

---
+++ todoapp/tests/test_home_view.py
@@ -0,0 +1,19 @@
+from todoapp.views import HomeView
+from django.test import TestCase
+from django.urls import resolve, reverse
+
+
+class HomeViewTest(TestCase):
+    def test_home_view_is_using_correct_view_class(self):
+        view = resolve(reverse('todoapp:home'))
+        self.assertIs(view.func.view_class, HomeView)
+
+    def test_home_view_is_rendering_correct_template(self):
+        response = self.client.get(reverse('todoapp:home'))
+        self.assertTemplateUsed(response, 'todoapp/view/home.html')
+
+    def test_home_view_title_is_rendering_correctly(self):
+        response = self.client.get(reverse('todoapp:home'))
+        title_needed = f'{response.context["title"]} | ToDo - App'
+        content = response.content.decode('utf-8')
+        self.assertIn(title_needed, content)
