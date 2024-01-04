ce285bc8411934c9e44fecbea52291df677d5ec8
Deivison Washington <deivisonwashington@hotmail.com>
2024-01-04 20:50:04 -0300

    style: mudanças no template

---
+++ global_static/global/js/default.js
@@ -0,0 +1,6 @@
+toggler = document.getElementById('toggle-bar');
+toggler.addEventListener('click', function(ev) {
+    content = document.getElementById('main-content-container')
+    is_collapsed = Boolean(content.getAttribute('sidebar-collapse').toString().toLowerCase() === 'true')
+    content.setAttribute('sidebar-collapse', !is_collapsed);
+});
