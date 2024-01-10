c7c1b9b87555987812c5b84d5ee646b01c37645b
Deivison Washington <deivisonwashington@hotmail.com>
2024-01-10 11:23:24 -0300

    test: refatorando testes

--- todoapp/tests/test_home_view.py
+++ todoapp/tests/test_home_view.py
@@ -1,9 +1,25 @@
 from todoapp.views import HomeTemplateView
+from utils.utils import create_user
+
 from django.test import TestCase
 from django.urls import resolve, reverse
 
 
 class HomeViewTest(TestCase):
+    def setUp(self):
+        self.user_data = {
+            'username': 'DummyUser',
+            'password': '#DummyPassword'
+        }
+        self.user = create_user(
+            self.user_data['username'],
+            self.user_data['password']
+        )
+        self.client.login(
+            username=self.user_data['username'],
+            password=self.user_data['password']
+        )
+
     def test_home_view_is_using_correct_view_class(self):
         view = resolve(reverse('todoapp:home'))
         self.assertIs(view.func.view_class, HomeTemplateView)
@@ -17,3 +33,9 @@ class HomeViewTest(TestCase):
         title_needed = f'{response.context["title"]} | ToDo - App'
         content = response.content.decode('utf-8')
         self.assertIn(title_needed, content)
+
+    def test_home_view_display_default_text_if_does_not_have_any_task(self):
+        response = self.client.get(reverse('todoapp:home'))
+        text_needed = 'Looks like you don\'t have any task'
+        content = response.content.decode('utf-8')
+        self.assertIn(text_needed, content)
