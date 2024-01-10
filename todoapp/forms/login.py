7cc64212a2c95cd49f10026261879da93bd373d5
Deivison Washington <deivisonwashington@hotmail.com>
2024-01-10 11:17:21 -0300

    formulário para realizar o login

---
+++ todoapp/forms/login.py
@@ -0,0 +1,26 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Type your username'
+            }
+        ),
+        error_messages={
+            'required': 'This field can not be empty.',
+        },
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Type your password'
+            }
+        ),
+        error_messages={
+            'required': 'This field can not be empty.'
+        }
+    )
