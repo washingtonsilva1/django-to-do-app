979a5b49018874a681d6eeb6a4effad3e6193002
Deivison Washington <deivisonwashington@hotmail.com>
2024-01-12 15:05:43 -0300

    criando TaskMixin para auxiliar testes

--- utils/utils.py
+++ utils/utils.py
@@ -1,8 +1,24 @@
 from django.contrib.auth.models import User
+from todoapp.models import Task
 
 
-def create_user(username='dummyuser', password='#JoeDoe21'):
-    return User.objects.create_user(
-        username=username,
-        password=password
-    )
+class TaskMixin:
+    def create_user(self, username='dummyuser', password='#JoeDoe21'):
+        return User.objects.create_user(
+            username=username,
+            password=password
+        )
+
+    def create_task(self,
+                    name='Task Example',
+                    desc='Task Description',
+                    is_completed=False,
+                    user=None):
+        if user is None:
+            user = self.create_user()
+        return Task.objects.create(
+            name=name,
+            description=desc,
+            is_completed=is_completed,
+            user=user
+        )
