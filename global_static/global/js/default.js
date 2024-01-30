dcf01a31d21de598b0dc87c50e060773a51b708b
Deivison Washington <deivisonwashington@hotmail.com>
2024-01-29 23:03:44 -0300

    removendo default.js

--- global_static/global/js/default.js
+++
@@ -1,8 +0,0 @@
-toggler = document.getElementById('toggle-bar');
-if(toggler) {
-    toggler.addEventListener('click', function(ev) {
-        content = document.getElementById('main-content-container')
-        is_collapsed = Boolean(content.getAttribute('sidebar-collapse').toString().toLowerCase() === 'true')
-        content.setAttribute('sidebar-collapse', !is_collapsed);
-    });
-}
