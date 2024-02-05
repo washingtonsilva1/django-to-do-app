40919e20c6db3085d24afc16ee4d2fbf80f62491
Deivison Washington <deivisonwashington@hotmail.com>
2024-02-05 17:06:02 -0300

    test: testando view de registro

---
+++ todoapp/tests/views/test_register_view.py
@@ -0,0 +1,29 @@
+from todoapp.views import RegisterTemplateView
+from utils.utils import TaskMixin
+from django.test import TestCase
+from django.urls import resolve, reverse
+from django.http import HttpResponse
+
+
+class RegisterViewTest(TestCase, TaskMixin):
+    def send_get(self) -> HttpResponse:
+        response = self.client.get(reverse('todoapp:register'))
+        return response
+
+    def test_register_view_is_using_correct_view_class(self):
+        view = resolve(reverse('todoapp:register'))
+        self.assertIs(view.func.view_class, RegisterTemplateView)
+
+    def test_register_view_is_rendering_correct_template(self):
+        response = self.send_get()
+        self.assertTemplateUsed(response, 'todoapp/view/register.html')
+
+    def test_register_view_redirects_to_home_if_user_is_logged_in(self):
+        user_data = {
+            'username': 'dummyuser',
+            'password': 'dummypassword'
+        }
+        self.create_user(**user_data)
+        self.client.login(**user_data)
+        response = self.send_get()
+        self.assertRedirects(response, reverse('todoapp:home'))
