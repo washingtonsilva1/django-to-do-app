0fe151b9ff4bb23aac0d9f3cb8ee74311cc1c6df
Deivison Washington <deivisonwashington@hotmail.com>
2024-02-14 16:47:30 -0300

    refactor: nome da view mudado

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('task/create/', views.TaskCreateView.as_view(), name='task_create'),
     path('task/delete/', views.TaskDeleteView.as_view(), name='task_delete'),
     path('task/update/', views.TaskUpdateView.as_view(), name='task_update'),
-    path('task/clear/', views.TasksClearView.as_view(), name='tasks_clear'),
+    path('task/delete/completed/',
+         views.CleanCompletedTasksView.as_view(),
+         name='tasks_clear'),
 ]
