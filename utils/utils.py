0ed233b769251feef3a7826a41682d33a455fd28
Deivison Washington <deivisonwashington@hotmail.com>
2024-02-14 16:48:37 -0300

    possibilitando que o sample tenha o status desejado

--- utils/utils.py
+++ utils/utils.py
@@ -24,14 +24,15 @@ class TaskMixin:
             user=user
         )
 
-    def create_task_sample(self, n=5, user=None):
+    def create_task_sample(self, n=5, is_completed=False, user=None):
         if user is None:
             user = self.create_user()
         tasks = []
         for i in range(1, n+1):
             tasks.append(self.create_task(
                 name=f'Task #{i}',
-                user=user
+                user=user,
+                is_completed=is_completed,
             ))
         tasks.reverse()
         return tasks
